141e4af912420df6594256ec82e64666babdae81
Storm <stricklandstorm10@gmail.com>
2018-07-31 10:05:07 -0400

    week3

---
+++ addingStyle/lib/js/main.js
@@ -0,0 +1,3 @@
+document.body.style.backgroundColor = hex = "#d3c26f";
+
+document.getElementById("name").style.textShadow = "5px 5px 1px #ff0000, 10px 10px 1px #ff0000";
---
+++ theBrush/lib/js/main.js
@@ -0,0 +1,3 @@
+Document.getElementById("lib/js/main.js").addEventListener("click", "rengar.png");
+Element.addEventListener("click", "rengar.png")
+alert("you have been deleted")
